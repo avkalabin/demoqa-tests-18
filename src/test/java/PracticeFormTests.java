866fd844227d2bb5e65de7f3d0d0050ea5f17c9c
Andrei Kalabin <a.kalabin@inbox.ru>
2023-02-20 20:02:20 +0500

    added "shouldHave" conditions

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,13 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
-import java.util.Date;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -24,16 +21,18 @@ public class PracticeFormTests {
         $("#userEmail").setValue("ivanov@email.com");
         $("label[for='gender-radio-1']").click();
         $("#userNumber").setValue("8800555555");
-        $("#dateOfBirthInput").setValue("11 Jan 1989");
         $("#subjectsInput").setValue("English").pressEnter().setValue("Computer Science").pressEnter();
         $("label[for='hobbies-checkbox-1']").click();
         $("#uploadPicture").uploadFromClasspath("file.png");
         $("#currentAddress").setValue("some address");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
-       // $("#submit").click();
-        //$("#output").shouldHave(text("Ivan Petrov"),text("ivanpetrov@ya.ru"),
-       //         text("some address"),text("anotherAddress"));
+        executeJavaScript("$('[id = dateOfBirthInput]').val('11 Jan 2000')");
+        $("#submit").click();
+        $(".table-responsive").shouldHave(text("Ivan Ivanov"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("file.png"));
 
 
     }
