42968b9c9558bb0588f836ff349dd525ee47c50e
Andrei Kalabin <a.kalabin@inbox.ru>
2023-10-15 18:20:14 +0500

    changed Listener

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,11 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import config.WebDriverConfig;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -35,6 +38,11 @@ public class TestBase {
         ));
 
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
