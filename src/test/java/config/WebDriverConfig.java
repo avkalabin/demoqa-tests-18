214a4139148d01d18693da31098b69db80ce554c
Andrei Kalabin <a.kalabin@inbox.ru>
2023-11-05 15:18:55 +0500

    added Config.Sources

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -2,7 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system:properties",
         "classpath:${env}.properties"
 })
 
